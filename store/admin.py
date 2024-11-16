5939c77a667b0db2e3cddd8aad799c3ceb4371f1
sam <sampurna.chapagainnn@gmail.com>
2024-11-16 19:30:25 +0000

    register models in admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Product)
+admin.site.register(Category)
+# admin.site.register(Order)
+# admin.site.register(OrderItem)
+admin.site.register(Cart)
+
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    # raw_id_fields=["product"]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    # list_display=["id", "product", "quantity"]
+    inlines=[OrderItemInline]
