623811a46357aff2563c4dd495c7939f93061178
sam <sampurna.chapagainnn@gmail.com>
2024-12-15 22:52:20 +0000

    update order model and add category id to product model

--- store/models.py
+++ store/models.py
@@ -16,16 +16,20 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=10, decimal_places=2)
     stock=models.PositiveIntegerField()
     image=models.ImageField(upload_to='products/', blank=True, null=True)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
 
 class Order(models.Model):
-    customer=models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at=models.DateTimeField(auto_now_add=True)
-    is_paid=models.BooleanField(default=False)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    full_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"Order {self.id} by {self.customer}"
     
